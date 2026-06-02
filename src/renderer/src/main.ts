d0ebb4df5e88c2fb7f56cf703937ef7ddc611692
Daniel GAVRILINE <daniel.gavriline@bluesoft-group.com>
2026-06-02 17:48:26 +0200

    feat(v1c): layout B + rail d'agents + console formatee (remplacement simple)

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,13 +1,20 @@
 import { mountTerminal } from './terminal'
+import { mountRail } from './rail'
 
 const root = document.getElementById('app')!
-root.innerHTML = '<div id="term" style="position:absolute;inset:0;"></div>'
+root.innerHTML = `
+  <div id="row">
+    <div id="term"></div>
+    <div id="console"></div>
+    <div id="rail"></div>
+  </div>`
 
 const cwd = 'C:\\Users\\daniel.gavriline\\Desktop\\Travail\\Claude apps\\DIFAI-HUB'
 
 async function boot(): Promise<void> {
   const tabId = await window.hub.newSession(cwd)
   mountTerminal(document.getElementById('term')!, tabId)
+  mountRail(tabId, document.getElementById('rail')!, document.getElementById('console')!)
 }
 
 boot()
